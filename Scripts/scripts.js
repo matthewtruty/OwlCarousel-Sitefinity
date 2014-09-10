9822ad6a9b5566c35a6c09e943cdfe41624ac363
matthewtruty0093 <mdtruty@gmail.com>
2014-09-10 09:09:53 -0400

    Create scripts.js

---
+++ Scripts/scripts.js
@@ -0,0 +1,26 @@
+ var carouselCount = 0;
+    $(".owl-carousel").each(function () {
+        $(this).attr("id", "owl-carousel" + carouselCount);
+        $('#owl-carousel' + carouselCount).owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            rewindNav: false,
+            lazyLoad: true,
+            navigation: true,
+            navigationText: ["Prev", "Next"],
+            // Responsive 
+            responsive: true,
+            responsiveRefreshRate: 200,
+            responsiveBaseWidth: window,
+            //Pagination
+            pagination: false
+        });
+        carouselCount++;
+    });
+ 
+    // Initialize FancyBox
+    $(".item a").fancybox();
